6aa51fef444eb0e3a1c3e1c8f587b3c0fbf89039
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-05-21 20:03:06 +0500

    adding links

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push("/dashboard");
     }
   }
 
@@ -19,9 +19,26 @@ class Landing extends Component {
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Developer Connector</h1>
                 <p className="lead">
-                  {' '}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
+                  <h1>Create by Mudassir Ijaz</h1>
+                  <a href="https://github.com/mudassir-ijaz" target="_blank">
+                    github
+                  </a>
+                  <a
+                    className="margin"
+                    href="https://medium.com/@ijazmudassir786"
+                    target="_blank"
+                  >
+                    medium
+                  </a>
+                  <a
+                    className="margin"
+                    href="http://mudassirijaz.pb.online/"
+                    target="_blank"
+                  >
+                    personal site
+                  </a>
                 </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
@@ -40,11 +57,11 @@ class Landing extends Component {
 }
 
 Landing.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(Landing);
